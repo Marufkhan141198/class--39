bfcafaa4828bdf745c332ed8f9aaef152aaf7f5f
Marufkhan141198 <khannmaruf141198@gmail.com>
2025-02-20 19:14:45 +0600

    js

---
+++ script.js
@@ -0,0 +1,49 @@
+//Determine summation.........
+
+function sum (x,y)
+{
+    let digit1 = x;
+    let digit2 = y;
+    let result = digit1 + digit2;
+    console.log("The sum  is " + result);
+}
+sum(20,30);
+sum(120,30);
+
+//Determine sub....
+
+function sub (x,y)
+{
+    let digit1 = x;
+    let digit2 = y;
+    let result = digit1-digit2;
+    console.log("The sub is "+ result);
+}
+sub(50,30);
+sub(100,50);
+
+//Dertermine muliplication..
+
+
+function mul (x,y)
+{
+    let digit1 = x;
+    let digit2 = y;
+    let result = digit1 * digit2;
+    console.log("The multiplication is "+ result);
+}
+mul(20,5);
+sub(25,50);
+
+//Determine division...
+
+function div (x,y)
+{
+    let digit1 = x;
+    let digit2 = y;
+    let result = digit1 / digit2;
+    console.log("The division is "+ result);
+}
+div(50,5);
+
+
